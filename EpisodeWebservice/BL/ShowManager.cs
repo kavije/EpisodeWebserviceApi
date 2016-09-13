aa715741e2c3ba0d9c146208b5baafacb2956626
kavije <kavithajayachandran@gmail.com>
2016-09-13 21:49:03 +1000

    Add project files.

---
+++ EpisodeWebservice/BL/ShowManager.cs
@@ -0,0 +1,47 @@
+﻿using EpisodeWebservice.DTO.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using static EpisodeWebservice.BL.IShowManager;
+
+namespace EpisodeWebservice.BL
+{
+    public class ShowManager: iShowManager
+    {
+        iShowManager _iShowManager;
+        /*future implementation of IOC container*/
+        public ShowManager(iShowManager iShowManager)
+        {
+            _iShowManager = iShowManager;
+        }
+        public ShowManager()
+        {
+            //_iShowManager = iShowManager;
+        }
+
+        /*Handles the logic of payload filtering*/
+        public ShowList GetShows(RootObject inputPayLoad)
+        {
+            if (inputPayLoad.payload == null)
+                return new ShowList();
+          
+           var results = inputPayLoad.payload.Where(x => x.drm == true && x.episodeCount > 0).Select(
+                g => new Reponse() {
+                    Image = g.image.showImage,
+                    Slug =g.slug,
+                    Title = g.title
+            });
+            var showList = new ShowList
+            {
+                response = results
+            };
+            return showList;
+        }
+       }
+
+
+
+    
+}
---
+++ EpisodeWebservice/DTO/ViewModels/Reponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpisodeWebservice.DTO.ViewModels
+{
+    public class Reponse
+    {
+        public string Image { get; set; }
+        public string Slug { get; set; }
+        public string Title { get; set; }
+    }
+
+  
+}
